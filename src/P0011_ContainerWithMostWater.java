5817388fdc4842147745b7046baf67fedc9527c9
kj84park <netom@naver.com>
2020-09-13 10:42:12 +0900

    Add P11

---
+++ src/P0011_ContainerWithMostWater.java
@@ -0,0 +1,18 @@
+public class P0011_ContainerWithMostWater {
+    public static void main(String[] args) throws Exception {
+
+        int [] arr = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(arr));
+    }
+    public static int maxArea(int[] height) {
+        int max = 0;
+
+        for (int i = 0 ; i < height.length ; i++){
+            for (int j = i ; j < height.length ; j++){
+                int wallHeight = Math.min(height[i],height[j]);
+                max = Math.max(max, wallHeight * Math.abs(i-j));
+            }
+        }
+        return max;
+    }
+}
