fba342366d5c03ada9906429d2c3bb5fd80c4e0e
kj84park <netom@naver.com>
2020-09-15 21:11:08 +0900

    Add P14

---
+++ src/P1014_LongestCommonPrefix.java
@@ -0,0 +1,30 @@
+public class P1014_LongestCommonPrefix {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(longestCommonPrefix(new String[]{"dog","racecar","car"}));
+    }
+    public static String longestCommonPrefix(String[] strs){
+
+        if(strs.length == 0){
+            return "";
+        }
+
+        String ret = strs[0];
+
+            for (int i = 0 ; i < strs.length ; i++ ){
+                for(int j = 0 ; j < ret.length(); j++) {
+                    if ((strs[i].length() > j) && ret.charAt(j) == (strs[i].charAt(j))) {
+
+                    } else {
+                        if(j == 0){
+                            return "";
+                        } else {
+                            ret = ret.substring(0,j);
+                        }
+                    }
+                }
+            }
+            return ret;
+    }
+}
