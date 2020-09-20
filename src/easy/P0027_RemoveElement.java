fc87ef9be0effd7f469411c75c3b5fa1459a88f0
kj84park <netom@naver.com>
2020-09-20 14:50:13 +0900

    Add P27.

---
+++ src/easy/P0027_RemoveElement.java
@@ -0,0 +1,20 @@
+package easy;
+
+import java.util.Arrays;
+
+public class P0027_RemoveElement {
+    public static void main(String[] args) throws Exception {
+        System.out.println(removeElement(new int[]{3,2,2,3},3));
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int count = 0;
+        for (int i = 0 ; i < nums.length ; i++){
+            if(nums[i] != val){
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
