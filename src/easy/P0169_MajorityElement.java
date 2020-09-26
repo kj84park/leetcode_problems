39a58eec9ebd9cd7ba8d340d198d52dbf4f8f039
kj84park <netom@naver.com>
2020-09-26 20:32:56 +0900

    Add P169.

---
+++ src/easy/P0169_MajorityElement.java
@@ -0,0 +1,29 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P0169_MajorityElement {
+    public static void main(String[] args) throws Exception {
+        System.out.println(majorityElement(new int[]{3,3,4}));
+    }
+
+    public static int majorityElement(int[] nums) {
+        Map<Integer,Integer> ret = new HashMap<>();
+        int result = 0;
+
+         for (int value : nums) {
+            if(!ret.containsKey(value)){
+                ret.put(value,1);
+            } else{
+                ret.put(value,ret.get(value)+1);
+            }
+            if(ret.get(value) > nums.length/2 ){
+                result = value;
+            }
+        }
+
+
+        return result ;
+    }
+}
--- src/medium/P0077_Combinations.java
+++ src/medium/P0077_Combinations.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class P0077_Combinations {
     public static void main(String[] args) throws Exception {
-        System.out.println(combine(4, 2));
+        System.out.println(combine(6, 3));
     }
 
     public static List<List<Integer>> combine(int n, int k) {
@@ -14,16 +14,32 @@ public class P0077_Combinations {
         return list;
     }
 
+//    public static void combination(int n, int k, List<List<Integer>> list, List<Integer> path, int start) {
+//        if (path.size() == k) {
+//            list.add(new ArrayList<>(path));
+//            return;
+//        }
+//        for (int i = start; i <= n; i++) {
+//            path.add(i);
+//            System.out.println(path);
+//            combination(n, k, list, path, i + 1);
+//            path.remove(path.size() - 1);
+//            System.out.println("?? : "+path);
+//            System.out.println("### : "+list);
+//        }
+//    }
+
     public static void combination(int n, int k, List<List<Integer>> list, List<Integer> path, int start) {
-        if (path.size() == k) {
+        if(k == path.size()){
             list.add(new ArrayList<>(path));
             return;
         }
-        for (int i = start; i <= n; i++) {
+
+        for (int i = start; i <= n ; i++){
             path.add(i);
-            combination(n, k, list, path, i + 1);
-//            System.out.println(path);
-            path.remove(path.size() - 1);
+            System.out.println("?? : "+path);
+            combination(n,k,list,path,i+1);
+            path.remove(path.size()-1);
         }
     }
 }
